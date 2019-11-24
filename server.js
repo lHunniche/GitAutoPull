c370678872599268501c522858726bb209c09c70
Lasse <lasse96-06@hotmail.com>
2019-11-24 15:36:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ function handlePrint(req, res, queryData) {
 }
 
 function handleGitPull(req, res, queryData) {
-    if (queryData.id === 1) {
+    if (queryData.id === "1") {
         shell.exec("pull.sh");
     }
     res.end("Pulling..")
