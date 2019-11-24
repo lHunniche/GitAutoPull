7716fcb175a1f5f46c74bd56b59f976b5bc2d189
Lasse <lasse96-06@hotmail.com>
2019-11-24 15:33:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ function handlePrint(req, res, queryData) {
         } 
     }
     
-    res.end("\nDone printing...");
+    res.end("\nDone printing");
 }
 
 function handleGitPull(req, res, queryData) {
