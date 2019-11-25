e891f657cdc3a4d6b4f4f579582a73b46e93362a
Lasse Hunniche Klevang <lasse96-06@hotmail.com>
2019-11-25 12:27:50 +0100

    Create pullDataScience.sh

---
+++ pullDataScience.sh
@@ -0,0 +1,4 @@
+cd /var/www/vegascatter/datascience_group8/Frontend/charts/dist
+sudo git reset --hard
+sudo git pull "https://lHunniche:pengepung22@github.com/Thorup/datascience_group8.git" master
+sudo npm install
