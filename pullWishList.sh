3e4ae2b306436a8255b1bf9db58b308f055718db
Lasse <lklev16@student.sdu.dk>
2020-08-26 13:58:59 +0200

    Update pullWishList.sh

--- pullWishList.sh
+++ pullWishList.sh
@@ -1,3 +1,3 @@
 cd /var/www/wishlist
-git reset --hard
-git pull "https://lHunniche:pengepung22@github.com/lHunniche/wishlist.git" master
+sudo git reset --hard
+sudo git pull "https://lHunniche:pengepung22@github.com/lHunniche/wishlist.git" master
