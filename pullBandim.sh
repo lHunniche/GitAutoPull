ef163b856ac459a95fbf27f0baac51669d66cf84
Lasse <lklev16@student.sdu.dk>
2021-02-26 23:13:06 +0100

    Update pullBandim.sh

--- pullBandim.sh
+++ pullBandim.sh
@@ -2,10 +2,10 @@
 
 cd /home/lklev16/github/Bandim-HDSS
 sudo git reset --hard
-sudo git pull "https://lHunniche:pengepung22@github.com/Xrosby/Bandim-HDSS.git" development
+sudo git pull "https://lHunniche:pengepung22@github.com/Xrosby/Bandim-HDSS.git"
 
-chmod u+x webserver/_misc/safe_dump.sh
-chmod u+x webserver/_misc/mysql2sqlite
+sudo chmod u+x webserver/_misc/safe_dump.sh
+sudo chmod u+x webserver/_misc/mysql2sqlite
 
 cd /home/lklev16/github/GitAutoPull
 bash build_bandim_react_app.sh /home/lklev16/github/Bandim-HDSS/bandim-app
