5f1a9b3845be77c29dca6e92de201c6fa52e8019
Lasse Hunniche Klevang <lklev16@hdss.bandim.sdu.dk>
2021-02-26 11:51:33 +0100

    my message

--- pullBandim.sh
+++ pullBandim.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /root/github/Bandim-HDSS/
+cd /home/lklev16/github/Bandim-HDSS
 sudo git reset --hard
 sudo git pull "https://lHunniche:pengepung22@github.com/Xrosby/Bandim-HDSS.git" development
 
 chmod u+x webserver/_misc/safe_dump.sh
 chmod u+x webserver/_misc/mysql2sqlite
 
-cd /root/github/GitAutoPull
-bash build_bandim_react_app.sh /root/github/Bandim-HDSS/bandim-data
+cd /home/lklev16/github/GitAutoPull
+bash build_bandim_react_app.sh /home/lklev16/github/Bandim-HDSS/bandim-app
--- run_bandim_flask_server.sh
+++ run_bandim_flask_server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-cd /root/github/Bandim-HDSS/webserver
+cd /home/lklev16/github/Bandim-HDSS/webserver
 . ./venv/bin/activate
 pip3 install -r ./_misc/requirements.txt
-python server.py -d
+python3.8 server.py -d
+
+
 # -d : for development branch (runs with SSL on port 5000)
 # -p/-m : for master branch (runs with SSL on port 443, and has debug mode disabled)
