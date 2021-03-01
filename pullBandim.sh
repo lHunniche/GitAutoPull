ef6649977db26d9b84b6b5a2abaf9adfa7b152ed
Lasse Hunniche Klevang <lklev16@student.sdu.dk>
2021-03-01 15:03:56 +0100

    hej

--- pullBandim.sh
+++ pullBandim.sh
@@ -6,6 +6,7 @@ sudo git pull "https://lHunniche:pengepung22@github.com/Xrosby/Bandim-HDSS.git"
 
 sudo chmod u+x webserver/_misc/safe_dump.sh
 sudo chmod u+x webserver/_misc/mysql2sqlite
+sudo chmod u+x SurveyApplication/gradlew
 
 cd /home/lklev16/github/GitAutoPull
 bash build_bandim_react_app.sh /home/lklev16/github/Bandim-HDSS/bandim-app
