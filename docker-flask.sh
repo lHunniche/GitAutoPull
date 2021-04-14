a41abb767d28304cf9ad940d81996aec9ae02bf1
Lasse <lklev16@student.sdu.dk>
2021-04-14 12:20:18 +0200

    Update docker-flask.sh

--- docker-flask.sh
+++ docker-flask.sh
@@ -22,9 +22,23 @@ if [[ $(docker ps -a | grep $CONTAINER) ]]; then
     echo "Container is already running. Restarting..."
     docker stop $CONTAINER
     docker rm $CONTAINER
-    docker run --name $CONTAINER --network="host" -d -p 443:443 -v react-app:/app/build hunniche/bandim-flask:1.0
+    docker run \
+        --name $CONTAINER \
+        --network="host" \
+        -d \
+        -p 443:443 \
+        -v react-app:/app/build \
+        -v master-log:/app/_logging \
+        hunniche/bandim-flask:1.0
     
 else
     echo "Container doesn't already exist. Creating new..."
-    docker run --name $CONTAINER --network="host" -d -p 443:443 -v react-app:/app/build hunniche/bandim-flask:1.0
+    docker run \
+        --name $CONTAINER \
+        --network="host" \
+        -d \
+        -p 443:443 \
+        -v react-app:/app/build \
+        -v master-log:/app/_logging \
+        hunniche/bandim-flask:1.0
 fi
